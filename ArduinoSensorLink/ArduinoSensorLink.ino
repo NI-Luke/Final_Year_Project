077bcbd20a25443d2c58bc9ece06f4b9b6dead18
Luke Hamill <lukehamill1@hotmail.com>
2018-04-10 14:37:14 +0100

    Add project files.

---
+++ ArduinoSensorLink/ArduinoSensorLink.ino
@@ -0,0 +1,64 @@
+/*
+*  eHealth sensor platform for Arduino and Raspberry from Cooking-hacks.
+*
+*  Copyright (C) Libelium Comunicaciones Distribuidas S.L.
+*  http://www.libelium.com
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*  a
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see http://www.gnu.org/licenses/.
+*
+*  Version:           2.0
+*  Design:            David Gascón
+*  Implementation:    Luis Martin & Ahmad Saad
+*/
+#include <PinChangeInt.h>
+#include <eHealth.h>
+
+int cont = 0;
+
+
+void setup() {
+
+
+	Serial.begin(115200);
+	eHealth.initPulsioximeter();
+
+	//Attach the inttruptions for using the pulsioximeter.
+	PCintPort::attachInterrupt(6, readPulsioximeter, RISING);
+}
+
+void loop() {
+
+	Serial.print("PRbpm: ");
+	Serial.print(eHealth.getBPM());
+	Serial.print("\n");
+
+	Serial.print("%SPo2: ");
+	Serial.print(eHealth.getOxygenSaturation());
+	Serial.print("\n");
+
+	delay(500);
+}
+
+
+//Include always this code when using the pulsioximeter sensor
+//=========================================================================
+void readPulsioximeter() {
+
+	cont++;
+
+	if (cont == 50) { //Get only of one 50 measures to reduce the latency
+		eHealth.readPulsioximeter();
+		cont = 0;
+	}
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private int count = 0;
+        private SerialPort stream = new SerialPort("COM3", 115200);
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(new ThreadStart(storeData));
+            button1.Enabled = false;
+            button2.Enabled = true;
+           
+            t.Start();
+
+            
+        }
+        Boolean stop = false;
+        private void storeData()
+        {
+            string line;
+            StreamWriter data = null;
+
+            if (stop == false)
+            {
+                data = new StreamWriter("something.csv");
+                stream.Open();
+            }
+
+            while (stop == false)
+            {
+                string PRBPM = stream.ReadLine();
+                string SPO2 = stream.ReadLine();
+
+                line = string.Format("{0},{1},", PRBPM, SPO2);
+                data.WriteLine(line);
+                data.Flush();
+                displayData(line);
+                
+            }
+            data.Close();
+            stream.Close();
+            stop = false;
+        }
+
+        private void displayData(string line)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            stop = true;
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+    }
+}
